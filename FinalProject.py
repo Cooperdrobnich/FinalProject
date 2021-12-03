fa362acf53d42aaab0f525070653d40e8ba97c71
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-03 13:42:22 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,11 @@
-#Name: Cooper Drobnich
-#Name: Adam Brenner
-#707 Backlot BOYZZ
+#FINAL PROJECT
+#GROUP NAME: 707 BACKLOT BOYZ
+#GROUP MEMBERS: COOPER DROBNICH & ADAM BRENNER
 
+import sqlite3
+import json
+import os
+import requests
+from bs4 import BeautifulSoup
 
+"""This file will create a database called lebron"""
