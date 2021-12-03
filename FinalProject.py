01aafc3dc81015e58b83966272d5f4a75fc6cb5c
Cooperdrobnich <drobnich@umich.edu>
2021-12-03 13:46:15 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -9,3 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 
 """This file will create a database called lebron"""
+def create_database(db):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db)
+    cur = conn.cursor()
+    return cur, conn
