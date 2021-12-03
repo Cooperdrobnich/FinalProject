a59122fc4d9c3b1e9a7656221aa528e753167be1
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-03 15:25:52 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 
 """This file will create a database called 'LebronAndMJStats.db' with all statistics from MJ and Lebron's """
 
+#=================LEBRON FUNCTIONS=================#
 def get_season_lebron():
     urls = ['https://www.basketball-reference.com/players/j/jamesle01.html']
     seasons_lebron = []
@@ -23,6 +24,32 @@ def get_season_lebron():
                 year = i.find('a').text
                 seasons_lebron.append(year)
     return seasons_lebron
+
+def get_age_lebron():
+    urls = ['https://www.basketball-reference.com/players/j/jamesle01.html']
+    age_lebron = []
+    for url in urls:
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        table = soup.find('table', {'id':'totals'})
+        rows = table.find_all('tr')
+        for row in rows[1:19]:
+            age = row.find_all('td', {'data-stat':'age'})
+            age_lebron.append(age[0].text)
+    return age_lebron
+
+def get_minutes_lebron():
+    urls = ['https://www.basketball-reference.com/players/j/jamesle01.html']
+    minutes_lebron = []
+    for url in urls:
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        table = soup.find('table', {'id':'totals'})
+        rows = table.find_all('tr')
+        for row in rows[1:19]:
+            age = row.find_all('td', {'data-stat':'mp'})
+            minutes_lebron.append(age[0].text)
+    return minutes_lebron
+
+#=================MJ FUNCTIONS=================#
 
 def get_season_mj():
     urls = ['https://www.basketball-reference.com/players/j/jordami01.html']
@@ -38,29 +65,29 @@ def get_season_mj():
                 seasons_mj.append(year)
     return seasons_mj
 
-def get_age_lebron():
-    urls = ['https://www.basketball-reference.com/players/j/jamesle01.html']
-    seasons_lebron = []
-    for url in urls:
-        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-        table = soup.find('table', {'id':'totals'})
-        rows = table.find_all('tr')
-        for row in rows[1:19]:
-            age = row.find_all('td', {'data-stat':'age'})
-            seasons_lebron.append(age[0].text)
-    return seasons_lebron
-
 def get_age_mj():
-    urls = ['https://www.basketball-reference.com/players/j/jamesle01.html']
-    seasons_mj = []
+    urls = ['https://www.basketball-reference.com/players/j/jordami01.html']
+    age_mj = []
     for url in urls:
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
         table = soup.find('table', {'id':'totals'})
         rows = table.find_all('tr')
         for row in rows[1:15]:
             age = row.find_all('td', {'data-stat':'age'})
-            seasons_mj.append(age[0].text)
-    return seasons_mj
+            age_mj.append(age[0].text)
+    return age_mj
+
+def get_minutes_mj():
+    urls = ['https://www.basketball-reference.com/players/j/jordami01.html']
+    minutes_mj = []
+    for url in urls:
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        table = soup.find('table', {'id':'totals'})
+        rows = table.find_all('tr')
+        for row in rows[1:15]:
+            age = row.find_all('td', {'data-stat':'mp'})
+            minutes_mj.append(age[0].text)
+    return minutes_mj
 
 def create_database(db):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -73,3 +100,5 @@ if __name__ == '__main__':
     mj_season_lst = get_season_mj()
     lebron_age_lst = get_age_lebron()
     mj_age_lst = get_age_mj()
+    lebron_minutes_lst = get_minutes_lebron()
+    mj_minutes_lst = get_minutes_mj()
