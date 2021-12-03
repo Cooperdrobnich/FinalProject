2a60a78a8cc9aac6fc1937ccb5183ebf405d5ffa
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-03 14:22:47 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -8,9 +8,23 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-"""This file will create a database called lebron"""
+"""This file will create a database called 'LebronAndMJStats.db' with all statistics from MJ and Lebron's """
+
+def get_season():
+    urls = ['https://www.basketball-reference.com/players/j/jamesle01.html','https://www.basketball-reference.com/players/j/jordami01.html']
+    seasons = []
+    for url in urls:
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        table = soup.find('table', {'id':'totals'})
+        rows = table.find_all('tr')
+
+
+
 def create_database(db):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db)
     cur = conn.cursor()
     return cur, conn
+
+if __name__ == '__main__':
+    season_lst = get_season()
