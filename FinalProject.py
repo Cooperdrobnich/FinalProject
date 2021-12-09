3cbf41b2ad0b3367c409b90464c4265475fe22f1
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-08 21:32:45 -0500

    nba players

--- FinalProject.py
+++ FinalProject.py
@@ -7,13 +7,12 @@ import json
 import os
 import requests
 from bs4 import BeautifulSoup
-import time
 
 """This file will create a database called 'Top100nbaStats.db' with all statistics from the top 100 players in the NBA for the 2021 - 2022 season """
 
 def get_player_names():
-    url = 'https://www.basketball-reference.com/leagues/NBA_2022_per_game.html'
-    seasons_lebron = []
+    url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
+    players = []
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     table = soup.find('table')
     rows = table.find_all('tr')
@@ -21,26 +20,75 @@ def get_player_names():
         player = row.find_all('td',{'data-stat':'player'})
         for i in player:
             name = i.find('a').text
-            seasons_lebron.append(name)
-    print(seasons_lebron)
-    
+            players.append(name)
+    return players
 
-def get_fgm_mj():
-    count = 1984
-    fgm_mj = []
-    for num in range(20):
-        url = f'https://www.balldontlie.io/api/v1/season_averages?season={count}&player_ids[]=2931'
-        response = requests.get(url)
-        data = response.json()
-        count += 1
+def get_team():
+    url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
+    team_abbr = []
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        team = row.find_all('td',{'data-stat':'team_id'})
+        for i in team:
+            team_id = i.find('a').text
+            team_abbr.append(team_id)
+    return team_abbr
+
+def get_minutes_played():
+    url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
+    minutes_played = []
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        mp = row.find_all('td',{'data-stat':'mp'})
         try:
-            for i in data:
-                fgm = data[i][0]['fgm']
-                fgm_mj.append(fgm)
+            minutes_played.append(int(mp[0].text))
         except:
             continue
-    return fgm_mj
+    return minutes_played
 
+def get_points():
+    url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
+    total_points = []
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        pts = row.find_all('td',{'data-stat':'pts'})
+        try:
+            total_points.append(int(pts[0].text))
+        except:
+            continue
+    return total_points
+
+def get_turnovers():
+    url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
+    turnovers = []
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        tov = row.find_all('td',{'data-stat':'tov'})
+        try:
+            turnovers.append(int(tov[0].text))
+        except:
+            continue
+    return turnovers
+
+def get_id_team():
+    id_abbr = {}
+    url ='https://www.balldontlie.io/api/v1/teams'
+    response = requests.get(url)
+    data = response.json()
+    for i in data['data']:
+        abbr = i['abbreviation']
+        division = i['division']
+        if abbr not in id_abbr:
+            id_abbr[abbr] = division
+    return id_abbr
 
 def create_database(db):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -51,6 +99,12 @@ def create_database(db):
 
 
 if __name__ == '__main__':
-    lebron_season_lst = get_player_names()
+    player_lst = get_player_names()
+    team_lst = get_team()
+    mp_lst = get_minutes_played()
+    pts_lst = get_points()
+    turnover_lst = get_turnovers()
+    id_abbr_dict = get_id_team()
+
 
     cur,conn = create_database('Top100nbaStats.db')
