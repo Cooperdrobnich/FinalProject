22d7ec948708491ad23b01f7d02a0507d9f10b6f
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-11 16:03:55 -0500

    Update Visualizations-Calculations.py

--- Visualizations-Calculations.py
+++ Visualizations-Calculations.py
@@ -5,15 +5,37 @@
 import sqlite3
 import matplotlib.pyplot as plt
 import os
-import numpy
+import numpy as np
 
-def get_player_dict(db_filename):
+def get_database(db_filename):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_filename)
     cur = conn.cursor()
-    cur.execute('SELECT  FROM PlayerStats')
+    return cur,conn
+    # cur.execute('''
+    # SELECT TeamDivision.division, COUNT(*)
+    # FROM PlayerStats
+    # JOIN TeamDivision
+    # ON PlayerStats.team = TeamDivision.team
+    # GROUP BY TeamDivision.division''')
+    # print(dict(cur))
+
+def points_minutes_viz(cur,conn):
+    points_lst = []
+    minutes_lst = []
+    cur.execute('''SELECT points, minutes_played FROM PlayerStats''')
+    for row in cur:
+        points_lst.append(row[0])
+        minutes_lst.append(row[1])
+    plt.scatter(minutes_lst,points_lst)
+    plt.xlabel("Total Minutes Played")
+    plt.ylabel("Total Points")
+    plt.title("Efficiency of Top 100 PLayers in NBA Season 21 - 22")
+    plt.tight_layout()
+    plt.show()
     
 
 
 if __name__ == "__main__":
-    player_dict = get_player_dict('Top100nbaStats.db')
+    cur,conn = get_database('Top100nbaStats.db')
+    viz1 = points_minutes_viz(cur,conn)
