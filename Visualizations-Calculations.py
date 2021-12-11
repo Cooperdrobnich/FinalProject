755f91aee6d100a3fbd108f58871306b408bcfca
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-11 14:55:21 -0500

    Create Visualizations-Calculations.py

---
+++ Visualizations-Calculations.py
@@ -0,0 +1,18 @@
+#FINAL PROJECT
+#GROUP NAME: 707 BACKLOT BOYZ
+#GROUP MEMBERS: COOPER DROBNICH & ADAM BRENNER
+
+import sqlite3
+import matplotlib.pyplot as plt
+import os
+
+def get_player_dict(db_filename):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_filename)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM PlayerStats')
+    print(dict(cur))
+
+
+if __name__ == "__main__":
+    player_dict = get_player_dict('Top100nbaStats.db')
