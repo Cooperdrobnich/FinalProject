9f3e2bcc00305fb5f1e3d3c331fa099e910e6750
adambrenner15 <84756950+adambrenner15@users.noreply.github.com>
2021-12-11 16:29:57 -0500

    Update Visualizations-Calculations.py

--- Visualizations-Calculations.py
+++ Visualizations-Calculations.py
@@ -30,12 +30,23 @@ def points_minutes_viz(cur,conn):
     plt.scatter(minutes_lst,points_lst)
     plt.xlabel("Total Minutes Played")
     plt.ylabel("Total Points")
-    plt.title("Efficiency of Top 100 PLayers in NBA Season 21 - 22")
+    plt.title("Efficiency of Top 100 Players in NBA Season 21 - 22")
     plt.tight_layout()
     plt.show()
+
+def get_most_efficient(cur,conn):
+    cur.execute('''SELECT name, points, minutes_played FROM PlayerStats''')
+    efficiency_lst = []
+    for row in cur:
+        efficiency_lst.append(row)
+    sorted_efficiency = sorted(efficiency_lst, key=lambda t: t[1]/t[2], reverse=True) 
+    most_efficent_player = sorted_efficiency[0][0]
+    most_efficent_ratio = sorted_efficiency[0][1] / sorted_efficiency[0][2]
+    print("The most efficient player in the NBA Top 100 is " + str(most_efficent_player) + " scoring " + str(round(most_efficent_ratio,3)) + " points per minute.")
     
 
 
 if __name__ == "__main__":
     cur,conn = get_database('Top100nbaStats.db')
     viz1 = points_minutes_viz(cur,conn)
+    calc1 = get_most_efficient(cur,conn)
