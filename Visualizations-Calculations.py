6e8193ec0ce5a2fc1cafd963570daf91cef8bf73
Cooperdrobnich <drobnich@umich.edu>
2021-12-12 14:28:51 -0500

    comm

--- Visualizations-Calculations.py
+++ Visualizations-Calculations.py
@@ -34,6 +34,20 @@ def points_minutes_viz(cur,conn):
     plt.tight_layout()
     plt.show()
 
+def turnovers_minutes_viz(cur,conn):
+    turnovers_lst = []
+    minutes_lst = []
+    cur.execute('''SELECT turnovers, minutes_played FROM PlayerStats''')
+    for row in cur:
+        turnovers_lst.append(row[0])
+        minutes_lst.append(row[1])
+    plt.scatter(minutes_lst,turnovers_lst)
+    plt.xlabel("Total Minutes Played")
+    plt.ylabel("Total Turnovers")
+    plt.title("Turnover Rate of Top 100 Players in NBA Season 21 - 22")
+    plt.tight_layout()
+    plt.show()
+
 def get_most_efficient(cur,conn):
     cur.execute('''SELECT name, points, minutes_played FROM PlayerStats''')
     efficiency_lst = []
@@ -43,10 +57,22 @@ def get_most_efficient(cur,conn):
     most_efficent_player = sorted_efficiency[0][0]
     most_efficent_ratio = sorted_efficiency[0][1] / sorted_efficiency[0][2]
     print("The most efficient player in the NBA Top 100 is " + str(most_efficent_player) + " scoring " + str(round(most_efficent_ratio,3)) + " points per minute.")
+
+def get_highest_turnover_rate(cur,conn):
+    cur.execute('''SELECT name, turnovers, minutes_played FROM PlayerStats''')
+    turnover_lst = []
+    for row in cur:
+        turnover_lst.append(row)
+    turnover_efficiency = sorted(turnover_lst, key=lambda t: t[1]/t[2], reverse=True) 
+    highest_turnover_rate = turnover_efficiency[0][0]
+    turnover_ratio = turnover_efficiency[0][1] / turnover_efficiency[0][2]
+    print("The player with the highest turnover rate in the NBA Top 100 is " + str(highest_turnover_rate) + " turning the ball over " + str(round(turnover_ratio,3)) + " times per minute.")
     
 
 
 if __name__ == "__main__":
     cur,conn = get_database('Top100nbaStats.db')
     viz1 = points_minutes_viz(cur,conn)
+    viz2 = turnovers_minutes_viz(cur, conn)
     calc1 = get_most_efficient(cur,conn)
+    calc2 = get_highest_turnover_rate(cur,conn)
